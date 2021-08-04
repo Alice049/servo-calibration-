9e23ddb89efd86ef48ccb26a23243d4743cb8130
Alice <69918118+Alice049@users.noreply.github.com>
2021-08-04 14:38:05 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,29 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    servos.P0.setAngle(0)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    servos.P0.setAngle(90)
+    basic.pause(2000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    servos.P0.setAngle(180)
+    basic.pause(2000)
+})
